22ab4240f116daf6ff0f77f90a4a5b324538e76b
miy20180601 <mo_hg1989@163.com>
2018-07-20 22:50:19 +0800

    修改文案'进入会客室'，点击通知进入qpp首页

--- app/src/main/java/com/mo/lawyercloud/jpush/MyReceiver.java
+++ app/src/main/java/com/mo/lawyercloud/jpush/MyReceiver.java
@@ -51,11 +51,11 @@ public class MyReceiver extends BroadcastReceiver {
 				Logger.d(TAG, "[MyReceiver] 用户点击打开了通知");
 
 				//打开自定义的Activity
-//				Intent i = new Intent(context, TestActivity.class);
-//				i.putExtras(bundle);
-//				//i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP );
-//				context.startActivity(i);
+				Intent i = new Intent(context, MainActivity.class);
+				i.putExtras(bundle);
+				//i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP );
+				context.startActivity(i);
 
 			} else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
 				Logger.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
